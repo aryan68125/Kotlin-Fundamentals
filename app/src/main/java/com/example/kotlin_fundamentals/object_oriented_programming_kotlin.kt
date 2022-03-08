9fdf2738b6fb21776b366426d198f5dc69abd4a3
aryan68125 <aryan68125@gmail.com>
2022-03-08 18:16:02 +0530

    Day 1 completed!

--- app/src/main/java/com/example/kotlin_fundamentals/object_oriented_programming_kotlin.kt
+++ app/src/main/java/com/example/kotlin_fundamentals/object_oriented_programming_kotlin.kt
@@ -4,13 +4,14 @@ package com.example.kotlin_fundamentals
 fun main()
 {
     //create an instance or an object of a class
-   var Name = Person("Aditya", "Kumar") //Here Name is an object of Person class
-
+   var Name = Person("Kandra", "Sunderland") //Here Name is an object of Person class
+   var defaultname = Person()// Here is another object of class Person this will display default name
+   var Name2 = Person(firstName = "Alice" ) // only assigning first name in the class Person
 }
 
 //a constructor allows us to add values to our objects when we create our objects of our class
 //constructor is not manditory
-class Person constructor(firstName : String , lastName : String)
+class Person constructor(firstName : String = "default first name", lastName : String = "default last name")
 {
     //class body
 
@@ -18,6 +19,7 @@ class Person constructor(firstName : String , lastName : String)
     //initializer initializes the objects of this class Person
     init
     {
-        println("Person created")
+        println("Person created" +
+        "First name = ${firstName} and Last name = ${lastName}")
     }
 }
