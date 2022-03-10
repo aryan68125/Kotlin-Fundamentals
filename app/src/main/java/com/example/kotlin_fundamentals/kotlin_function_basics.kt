0e96dbd0a24521aa84428418d6cd76aade1a1892
aryan68125 <aryan68125@gmail.com>
2022-03-10 12:11:46 +0530

    Day 2 Begins!

--- app/src/main/java/com/example/kotlin_fundamentals/kotlin_function_basics.kt
+++ app/src/main/java/com/example/kotlin_fundamentals/kotlin_function_basics.kt
@@ -13,6 +13,9 @@ fun main()
     //calling average function
     var result_avg = avg(25.256,36.5596)
     println("average of a and b is  = ${result_avg}")
+
+    //calling my_Function funtion
+    my_Function(420)
 }
 
 //creating a function in kotlin
@@ -34,4 +37,13 @@ fun addup(a: Double, b: Double): Double
 fun avg(a:Double, b: Double):Double{
     var result = (a+b)/2
     return result
+}
+
+//function demonstrating parameter
+//a: Int is a Parameter
+fun my_Function(a:Int)
+{
+    //a is a variable
+    //var a = a; //if comment out this line then ${a} will be the a:Int parameter
+    println("parameter passed in my_function function is = ${a}")//here a is the variable which will be printed
 }
--- app/src/main/java/com/example/kotlin_fundamentals/object_oriented_programming_kotlin.kt
+++ app/src/main/java/com/example/kotlin_fundamentals/object_oriented_programming_kotlin.kt
@@ -7,6 +7,17 @@ fun main()
    var Name = Person("Kandra", "Sunderland") //Here Name is an object of Person class
    var defaultname = Person()// Here is another object of class Person this will display default name
    var Name2 = Person(firstName = "Alice" ) // only assigning first name in the class Person
+
+    //calling stateHobbie function within the class Person
+    Name.stateHobby()
+    //you can change what the Hobbie function in the Person class displays on the console
+    //So now we can have a unique hobbie for every single person using Person class and it's methods
+    Name.hobbie = "I love to fuck Akd"
+    Name.stateHobby()
+
+    var Name3 = Person(firstName ="Liah", lastName = "Gotti",19) //Name3, Name2, defaultname, Name are all objects of Person class( class = Blue print)
+    Name3.hobbie = "I love to spend time with Akd" //hobbie is a property of Person class
+    Name3.stateHobby() //stateHobby() is the method of Person class
 }
 
 //a constructor allows us to add values to our objects when we create our objects of our class
@@ -15,11 +26,33 @@ class Person constructor(firstName : String = "default first name", lastName : S
 {
     //class body
 
+    //Member Variable - Properties
+    var age : Int? = null //age will be nullable
+    var hobbie: String = "watch Amazon Prime"
+
     //init is the initializer
     //initializer initializes the objects of this class Person
+    //initializes our first or primary constructor
     init
     {
         println("Person created" +
         "First name = ${firstName} and Last name = ${lastName}")
+    }
+
+    //create a constructor here
+    //To be honest it is a secondary constructor
+    //A constructor is also a member (Member constructor)
+    constructor(firstName: String, lastName : String, age: Int): this(firstName,lastName){
+        //this.age means we are refering to the member variable of this class
+        //age means the variable that is used to assign the age passed in the age from the class parameter to the member variable
+        this.age = age
+    }
+
+    //member function -> Methods
+    //Methods are the functions within a class
+    fun stateHobby()
+    {
+        println("My hobby is ${hobbie}")
+        println("My age is = ${age}")
     }
 }
