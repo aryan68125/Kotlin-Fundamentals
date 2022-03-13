be7f5fdc038f3036e80d64675270cb85cc6f32d7
aryan68125 <aryan68125@gmail.com>
2022-03-13 14:44:26 +0530

    Sets and Maps!

--- app/src/main/java/com/example/kotlin_fundamentals/sets_kotlin.kt
+++ app/src/main/java/com/example/kotlin_fundamentals/sets_kotlin.kt
@@ -22,6 +22,24 @@ fun main()
     val newFruits = fruits.toMutableList()
     newFruits.add(4,"Melon")
     newFruits.add(5,"Water Melon")
+    newFruits.add(6, "Pear")
     println("Printing newFruits set which is mutable")
     println(newFruits)
+
+    //MAPS
+    //Map is a type of collection that holds the data in form of key value pair just like a dictionary in python
+    //NOTE : HERE WE ARE USING NUMBERS TO "STRINGS" BUT YOU CAN ALSO USE THIS MAP FOR NUMBERS TO "OBJECTS" SO YOU ARE NOT LIMITED HERE
+    println()
+    println("Creating a map")
+    val daysOfTheWeek = mapOf(1 to "Monay", 2 to "Tuesday", 3 to "Tuesday", 4 to "Thursday" , 5 to "Friday", 6 to "Saturday")
+    println("Printing just one item")
+    println(daysOfTheWeek[2]) // printing only one item in a map
+
+    //now printing all the items in a map using for loop
+    println()
+    println("Printing all the items in a map")
+    for(key in daysOfTheWeek.keys)
+    {
+        println("$key is to ${daysOfTheWeek[key]}")
+    }
 }
