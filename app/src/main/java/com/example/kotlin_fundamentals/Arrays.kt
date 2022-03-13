5da03575030b8c0b21a954e8f1c081547e991d1a
aryan68125 <aryan68125@gmail.com>
2022-03-13 11:47:28 +0530

    Arrays!

--- app/src/main/java/com/example/kotlin_fundamentals/Arrays.kt
+++ app/src/main/java/com/example/kotlin_fundamentals/Arrays.kt
@@ -12,7 +12,7 @@ fun main()
         println(numbers[i])
         i++
     }
-
+    println()
     println("Printing numbers2 array")
     val numbers2 = intArrayOf(1,2,3,4,5,6,7,8,9,10)
     var j=0;
@@ -21,7 +21,8 @@ fun main()
         println(numbers2[j])
         j++
     }
-
+    println()
+    println("Print the number3 array")
     val numbers3  = arrayOf(1,2,3,4,5,6,7,8,91,0,128,256,64,32,16,8,2,4,1024,512,2048)
     //another way of printing the numbers
     //contentToString() converts the array contents into a string after which the print statement displays the numbers
@@ -32,6 +33,45 @@ fun main()
     //Honestly it reminds me of how python handles the list in a loop the syntax is almost the same which is good thing I guess
     for(element in numbers3)
     {
-        println(element)
+        println("${element}")
+    }
+    println()
+    //accessing the elements of an array using the indexes
+    //NOTE:-> THE INDEX IN HERE WILL ALWAYS START WITH 0 THAT MEANS IF WE WANT TO ACCESS THE 4TH ELEMENT THEN WE NEED TO PASS IN 4-1=3 IN THE numbers3[index]
+    println("element in index 3 an number3 array : ${numbers3[3]}")
+
+    //making changes to numbers 3 array
+    //NOTE ARRAYS HAVE FIXED LENGTH
+    numbers3[0] = 7150
+    numbers3[1] = 420
+    numbers3[3] = 117
+    println("Numbers3 array after making changes to it :->")
+    println("${numbers3.contentToString()}")
+
+    //another array example this time it will be of String type
+    println()
+    println("Print the array of String type that stores week days ")
+    val days = arrayOf("sunday","monday","tuesday","wednesday","thursday","friday","saturday")
+    println(days.contentToString())
+
+    //creating an array using a data class Fruit
+    println()
+    println("printing the fruits array that takes data from the data class Fruit")
+    val fruits = arrayOf(Fruit("Apple",2.5), Fruit("banana", 3.5))
+    println(fruits.contentToString())
+    //access individual items of fruits array
+    for(index in fruits.indices)
+    {
+        //${fruits[index].name} will get the elements in the array
+        // $index will the index of the elements of the array
+        println("${fruits[index].name} is in index $index")
+    }
+    //another and more simpler way to access the elements of the fruits array if you do not want to access the indexs of the elements of the array then
+    println()
+    for(fruit in fruits)
+    {
+        println("${fruit.name}")
     }
 }
+
+data class Fruit(val name:String, val price:Double)
