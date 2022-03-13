d03ddf1c2a5f9dd187d98161ecb303eba56ae9a3
aryan68125 <aryan68125@gmail.com>
2022-03-13 15:10:24 +0530

    ArrayList

---
+++ app/src/main/java/com/example/kotlin_fundamentals/ArrayList_Kotlin.kt
@@ -0,0 +1,25 @@
+package com.example.kotlin_fundamentals
+
+fun main()
+{
+    /*
+       - ArrayList are used to create dynamic arrays in a program
+       - ArrayList is used to create a dynamic array which means the size of an array can be increased or decreased according
+         to the need of the programmer OR developer
+       - ArrayList class provides both read and write functionalities
+       - ArrayList follows the sequence of insertion order
+       - ArrayList is non Synchronized and it may contain duplicate elements
+
+       - Constructor of an ArrayList Looks like this ArrayList<E>(): Its used to create an empty ArrayList
+       - ArrayList(capacity : Int): Its used to create an ArrayList of specified capacity
+       - ArrayList(elements: Collection<E>):Its used to create an ArrayList filled with the elements of a collection
+
+       Functions of ArrayList
+       open fun add(element:E):Boolean -> Used to add the specific element into the collection
+       open fun clear() -> Used to remove all elements from the collection
+       open fun get(index:Int):E->Used to return the elements at specified index in the list
+       open fun remove(element:E):Boolean -> Used to remove a single instance of the specific element
+       from the collection, if it is available.
+     */
+
+}
